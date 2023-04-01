b4803a4c6e06b5e4323c37041c1d4ac65010c3cc
Roy <royrmagdaong@gmail.com>
2023-04-01 20:42:51 +0800

    add login, about page, and next link router to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import styles from '@/styles/NavBar.module.css'
+import Link from 'next/link';
 
 const NavBar = () => {
     return (
@@ -6,9 +7,15 @@ const NavBar = () => {
             <nav className={styles.navbar}>
                 <h1 className={styles.title}>Job Hunt</h1>
                 <ul className={styles["nav-ul"]}>
-                    <li className={styles["search-jobs"]}>Search Job</li>
-                    <li className={styles["about"]}>About</li>
-                    <li className={styles["login"]}>Login</li>
+                    <li>
+                        <Link href="/" className={styles["search-jobs"]}>Search Jobs</Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className={styles["about"]}>About</Link>
+                    </li>
+                    <li>
+                        <Link href="/login" className={styles["login"]}>Login</Link>
+                    </li>
                 </ul>
             </nav>
         </div>
