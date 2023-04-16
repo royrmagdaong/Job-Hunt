3e09b8b405a9469a63cba0420477a9ce7725d925
Roy <royrmagdaong@gmail.com>
2023-04-16 16:59:02 +0800

    protect routes by roles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,17 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
-    const [userData, setUserData] = useState({})
+    const [userData, setUserData] = useState({
+        role: ''
+    })
 
     const { push } = useRouter();
 
     useEffect(()=>{
         const user = localStorage.getItem('user');
-        setUserData(JSON.parse(user))
+        if(user){
+            setUserData(JSON.parse(user))
+        }
         console.log(JSON.parse(user))
     }, [])
 
@@ -20,20 +24,45 @@ const NavBar = () => {
         push('/login')
     }
 
+    // const checkIfLoggedIn = () => {
+
+    //     if(!userData){
+    //         return 
+    //     }else{
+    //         if(userData.role === 'applicant'){
+    //             return <Link href="/" className={styles["search-jobs"]}>Search Jobs</Link>
+    //         }else if(userData.role === 'recruiter'){
+    //             return <Link href="/" className={styles["search-jobs"]}>post Job</Link>
+    //         }
+    //     }
+        
+    // }
+
     return (
         <div className={styles['nav-container']}>
             <nav className={styles.navbar}>
                 <h1 className={styles.title}>Job Hunt</h1>
                 <ul className={styles["nav-ul"]}>
                     <li>
-                        <Link href="/" className={styles["search-jobs"]}>Search Jobs</Link>
+                        {
+                            !userData['role']?<Link href="/" className={styles["search-jobs"]}>Search Jobs</Link>:''
+                        }
+                        {
+                            userData['role']==='applicant'?<Link href="/" className={styles["search-jobs"]}>Search Jobs</Link>:''
+                        }
+                        {
+                            userData['role']==='recruiter'?<Link href="/post-job" className={styles["search-jobs"]}>Post Job</Link>:''
+                        }
+                        {
+                            userData['role']==='admin'?<Link href="/users" className={styles["search-jobs"]}>Users</Link>:''
+                        }
                     </li>
                     <li>
                         <Link href="/about" className={styles["about"]}>About</Link>
                     </li>
                     <li>
                         {
-                            userData?<span onClick={logout} className={styles["login"]}>Logout</span> : <Link href="/login" className={styles["login"]}>Login</Link> 
+                            userData.role?<span onClick={logout} className={styles["login"]}>Logout</span> : <Link href="/login" className={styles["login"]}>Login</Link> 
                         }
                         
                     </li>
